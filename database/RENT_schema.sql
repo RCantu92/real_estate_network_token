673257b1abbad934e8ce1eb6b04eb85190da285b
Mina Kasrai <minak@rice.edu>
2020-01-20 12:48:56 -0600

    RENT database

---
+++ database/RENT_schema.sql
@@ -0,0 +1,58 @@
+CREATE TABLE tblowner_info (
+	owner_code SERIAL PRIMARY KEY,
+	first_name VARCHAR (30),
+	last_name VARCHAR (30),
+	wallet_address VARCHAR (50)
+);
+
+CREATE TABLE tblzip_codes (
+	zip_code INT PRIMARY KEY,
+	city VARCHAR,
+	state VARCHAR,
+	county VARCHAR
+);
+
+CREATE TABLE tblstatus_codes (
+	status_code SERIAL PRIMARY KEY,
+	status VARCHAR (30)
+);
+
+CREATE TABLE tblbuyer_info (
+	buyer_code SERIAL PRIMARY KEY,
+	wallet_address VARCHAR (50)
+);
+
+CREATE TABLE tblsale_info (
+	sale_code SERIAL PRIMARY KEY,
+	status_code INT NOT NULL,
+	owner_code INT NOT NULL,
+	listing_price MONEY,
+	sale_price MONEY,
+	buyer_code INT,
+	sale_date DATE,
+	transaction_hash VARCHAR (50),
+	FOREIGN KEY (status_code) REFERENCES tblstatus_codes(status_code),
+	FOREIGN KEY (owner_code) REFERENCES tblowner_info(owner_code),
+	FOREIGN KEY (buyer_code) REFERENCES tblbuyer_info(buyer_code)
+);
+
+CREATE TABLE tblproperty_info (
+	property_code INT PRIMARY KEY,
+	owner_code INT NOT NULL,
+	street_address VARCHAR (50),
+	zip_code INT NOT NULL,
+	subdivision VARCHAR,
+	legal_description VARCHAR,
+	bedrooms INT,
+	full_baths INT,
+	half_baths INT,
+	garage VARCHAR (30),
+	stories DECIMAL,
+	year_built INT,
+	building_sqft INT,
+	lot_sqft INT,
+	annual_maintenance_fee MONEY,
+	token_hash VARCHAR (50),
+	FOREIGN KEY (owner_code) REFERENCES tblowner_info(owner_code),
+	FOREIGN KEY (zip_code) REFERENCES tblzip_codes(zip_code)
+);
